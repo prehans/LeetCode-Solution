515fa2d4f3862ef0bad415dfc3aa058cf085cc92
Prehans Gupta <85963909+prehans@users.noreply.github.com>
2022-07-22 13:45:31 +0530

    Update 219-contains-duplicate-ii.cpp

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -3,12 +3,6 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
       
         map<int , int>mp;
-       /* for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]=i;
-        }
-        if(mp.size()<=1)
-            return false;*/
         for(int i=0;i<nums.size();i++)
         {
             if(mp.find(nums[i])!=mp.end())
@@ -18,7 +12,6 @@ public:
                 if(abs(i-it->second)<=k)
                    return true;
             }
-                  // mp.insert(nums[i], i);
             mp[nums[i]]=i;
         }
         return false;
