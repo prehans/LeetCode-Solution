ea9a13c80e75e189de6b9c5861edabaa3451e220
Prehans Gupta <85963909+prehans@users.noreply.github.com>
2022-08-23 10:11:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.30%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+       
+         if(n<=0)
+             return false;
+        while(n>0)
+        {
+            if(n==1)
+                return true;
+            if(n%4!=0)
+                return  false;
+            n=n/4;
+        }
+        return true;
+    }
+};
