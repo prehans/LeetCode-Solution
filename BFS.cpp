2c0904366914a5a637921346e1e22a7c6fdee49e
rahul-gargcoder <96135903+rahul-gargcoder@users.noreply.github.com>
2022-10-12 12:44:41 +0530

    Graph algos

--- BFS.cpp
+++ BFS.cpp
@@ -1,45 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bfs(vector<vector<int>> &adjList,int source,int N){
-	vector<bool> visited(N,false);
-	
-	queue<int> q;
-	visited[source]=true;
-	q.push(source);
-	
-	while(!q.empty()){
-		int currentNode=q.front();
-		cout<<"Visited: "<<currentNode<<endl;
-		q.pop();
-		int totalNeighbour = adjList[currentNode].size();
-		
-		for(int i=0;i<totalNeighbour;i++){
-			int neighbourNode = adjList[currentNode][i];
-			
-			if(visited[neighbourNode]==false){
-				visited[neighbourNode]=true;
-				q.push(neighbourNode);
-			}
-		}
-	}
+void bfs(vector<vector<int>> &graph, int source, vector<bool> &vis)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for (int neigh : graph[curr])
+            if (!vis[neigh])
+            {
+                q.push(neigh);
+                vis[neigh] = true;
+            }
+    }
 }
 
-int main(){
-	
-	int N=6;
-	
-	vector<vector<int>> adjList(N);
-	
-	adjList[0].push_back(1);
-	adjList[0].push_back(2);
-	adjList[0].push_back(4);
-	
-	adjList[1].push_back(3);
-	adjList[2].push_back(3);
-	adjList[3].push_back(4);
-	adjList[3].push_back(5);
-	
-	bfs(adjList,0,N);
-	
+int main()
+{
+    int n, e;
+    cout << "Enter the Number of Nodes : " << endl;
+    cin >> n;
+    cout << "Enter the number of Edges : " << endl;
+    cin >> e;
+    vector<vector<int>> graph(n);
+    vector<bool> vis(n, false);
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+            bfs(graph, i, vis);
+    }
+    return 0;
 }
